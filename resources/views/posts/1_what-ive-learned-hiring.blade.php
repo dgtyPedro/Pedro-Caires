e53e550826c0631f371317d96be92dcd99768db0
dgtyPedro <pedrogcs0104@gmail.com>
2022-02-21 22:03:23 -0300

    post 01 fixed

--- resources/views/posts/1_what-ive-learned-hiring.blade.php
+++ resources/views/posts/1_what-ive-learned-hiring.blade.php
@@ -3,28 +3,16 @@
 What I learned hiring other developers.
 @endsection
 @section('content')
-I work at a newborn startup, and only people that had work at a newborn startup really knows what is like it. You become basically 
-a fullstack employee, fullfiling the work of designers, tech leaders and even doing HR stuff. I already worked on hiring 3 other devs and I learn
-a lot of stuff from it. Here's what I got.
+I work at a newborn startup, and only people who have worked at a newborn startup really know what it's like it. You basically become a fullstack employee, filling in for designers tech leads, and even doing HR. I have already worked on hiring 3 other developers and learned a lot along the way. Here is what I learned.
 
 <h3>GitHub is one of the most important tool that a Developer can use</h3>
-<p>In the initial days most developers ignore the use of GitHub. They think that just because they don't have something "cool" to 
-    show it they just don't need to show something, that's completely wrong. When I did the pre-selection of the candidates I often 
-    prefered a dev that did have a populated GitHub, but that didn't have any previous experience than a dev with previous experiences but 
-    no content at their GitHub. When I see someone with an organized, well-documented GitHub I can tell with a few seconds what the person is and isn't capable of, 
-    when a candidate don't have a good GitHub I really can't tell what are the capabilities of the person. Of course that people lie 
-    and the GitHub/GitLab can't serve as a proof of capability, but it can be a great indicator.  
+<p>In the early days, most developers ignore using GitHub. They think that just because they do not have anything "cool" to show, they do not have to show anything, but that's completely wrong. When I was shortlisting candidates, I would often choose a developer who had a populated GitHub but no previous experience over a developer with previous experience but no content on their GitHub. When I see someone with a well- organized, well-documented GitHub, I can tell within seconds what the person can and cannot do. If a candidate does not have a good GitHub, I can not assess the person's skills. Of course, people lie and GitHub/GitLab cannot serve as a proof of skills, but it can be a good indicator.
 </p>
 <h3>Work your social skills</h3>
-<p>People at the technology area can be very awkward and that's okay! But you will need to know how to comunicate, when you stays too quiet 
-    at an interview you pass the look of "do not care" type of person, and unless you are a kind of genius that will probably cut you from the 
-    job chance really sooner that expected, smile and show interest at the job you are applying for.
+<p>People in the technology field can be very awkward and that's okay! But you will need to know how to communicate. If you stays too quiet during an interview, you'll come across as a person who does not care, and unless you are a genius, you'll probably be eliminated from the job opportunity sooner than expected.
 </p>
-<h3>Know what are you doing but don't be afraid of</h3>
-<p>Being ready is really different of knowing what you will need to do everyday. The problem with most developers is that they know what they "need" to know but they 
-    are not prepared to face something that they don't know yet, doesn't mean that if you just ended a Z tech course you will get the job, what make a good dev a GREAT dev is not 
-    what they know now, it is what they can know. We dev's can know everything, but still some of us limit themselves to only knowing X and doing Y, more important that 
-    knowing what you need to know to pass a job interview is know that you will need to learn some new things.
+<h3>Know what are you doing but don't be afraid of the need to learn new thing</h3>
+<p>Being ready is very different than knowing what you need to do every day. The problem with most developers is that they know what they "need" to know, but they are not prepared to deal with something they do not yet know. That does not mean you'll get the job if you just finished a Tech course. What makes a good developer a GREAT developer is not what they know now, but what they can know. We developers can know everything, but still some of us limit themselves to only knowing X and doing Y. More important than knowing what you need to know to pass an interview is knowing that you need to learn new things.
 </p>
 <br/>
 @endsection
