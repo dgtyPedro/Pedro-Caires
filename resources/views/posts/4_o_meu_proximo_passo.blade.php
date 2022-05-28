9e967efeb7f5f70931ce47a30480dd9e3cedff8f
dgtyPedro <pedrogcs0104@gmail.com>
2022-05-28 10:45:29 -0300

    pre post

--- resources/views/posts/4_o_meu_proximo_passo.blade.php
+++ resources/views/posts/4_o_meu_proximo_passo.blade.php
@@ -1,6 +1,6 @@
 @extends('index')
 @section('title')
-O meu próximo passo como desenvolvedor (e que muitos deveriam seguir) [PT-BR]
+O meu próximo passo como desenvolvedor [PT-BR]
 @endsection
 @section('content')
 <p>Eu pretendia começar a postar só em inglês nesse blog, mas por conta de ser uma postagem com cunho pessoal vou estar escrevendo 
@@ -9,54 +9,59 @@ O meu próximo passo como desenvolvedor (e que muitos deveriam seguir) [PT-BR]
 <style>p{margin:0!important;}</style>
 <br/>
 <h2>O Background</h2>
-<p>
-    Eu trabalho atualmente como Fullstack Web Developer em uma startup. Tive um crescimento notório nos meus primeiros mêses e rapidamente
-    conquistei meu lugar na empresa, quando entrei, ela ainda estava no início e até mesmo um Júnior (como eu na época) acabava 
-    recebendo grandes responsabilidades/tarefas, nesse momento ou o desenvolvedor não aguenta a pressão e acaba sendo desligado ou ele 
-    segura o tranco e ganha posição de destaque nessa empresa, esse último caso foi oque aconteceu comigo. Não acho que consegui 
-    por eu ser necessariamente um gênio e sim por estar acostumado a lidar com pressões e as inúmeras adversidades que 
-    podem aparecer nessas situações, recebi um certo prestígio dentro da empresa e passei a tocar projetos sozinhos, tinha a flexibilidade de fazer o que quiser 
-    quando quiser e tinha o conforto de saber que tudo estava dentro do meu controle, estava com o sentimento que o pior já tinha passado.
-    Acredito que essa é a história de muitos desenvolvedores e que grande parte passa por essa situação atualmente.
+<p style="text-indent: 5vw;">
+    Olá, sou o Pedro e hoje vou estar contando um pouco de um sentimento que vem me assombrando a um certo tempo.
+    Trabalho atualmente como Fullstack Web Developer em uma startup de soluções de TI, tive um crescimento notório nos meus primeiros mêses e rapidamente
+    conquistei meu lugar na empresa. Quando entrei, ela havia acabado de ser criada e isso culminou em eu, um dev Júnior na época, receber 
+    grandes responsabilidades/tarefas, quando essa situação ocorre ou o desenvolvedor não aguenta a pressão e acaba sendo desligado ou ele 
+    segura o tranco e ganha posição de destaque nessa empresa, esse último caso foi oque aconteceu comigo, não acho que consegui 
+    por eu ser necessariamente um gênio e sim por ter uma facilidade em aprender coisas no "calor do momento", com isso 
+    recebi um certo prestígio dentro da empresa e passei a tocar projetos sozinhos, tenho a flexibilidade de fazer o que quiser 
+    quando quiser e tenho o conforto de saber que tudo está dentro do meu controle, senti por um momento que o pior no processo de virar um desenvolvedor já tinha passado.
+    Acredito que essa é a história de muitos desenvolvedores e uma situação que grande parte, assim como eu, passa por essa situação.
 </p>
 <br/>
 <h2>A situação</h2>
-<p>Por mais que a programação seja uma área
-    altamente procurada nos dias atuais cada vez fica mais difícil conseguir uma oportunidade de emprego no início, 
+<p style="text-indent: 5vw;">Por mais que a programação seja uma área
+    altamente procurada nos dias atuais, cada vez fica mais difícil conseguir uma oportunidade de emprego no início, 
 a concorrência é extremamente alta para essas vagas de nível Júnior e já se foram os tempos onde um 
 certificado e vontade de aprender bastava, para realmente entrar no jogo se vão muitas horas de estudo e de projetos, eu passei por isso e sei que  
- quando conseguimos de fato a primeira oportunidade, o primeiro contrato, o primeiro dia de pagamento nós desenvolvedores ficamos extasiados. Agora a parte 
-mais difícil que era de fato conseguir uma oportunidade e se manter nela já foi, já se provamos e temos que nos preocupar somente 
-em trabalhar e mostrar serviço, pelo menos é o que pensamos, e é aí que surge uma grande armadilha que muitos, inclusive eu, caem. A estagnação. O dia que ficamos 
-para trás sempre vai chegar
+ quando de fato conseguimos a primeira oportunidade; o primeiro contrato; o primeiro dia de pagamento; nós desenvolvedores ficamos extasiados. Agora a parte 
+mais difícil que era conseguir uma oportunidade e se manter nela já foi, já se provamos e temos que nos preocupar somente 
+em trabalhar e mostrar serviço, pelo menos é o que pensamos, e é aí que surge uma grande armadilha que muitos caem. A estagnação pelo conforto. 
 </p>
 <br/>
 <h2>O problema</h2>
-<p>O conforto que um programador como eu (no qual foi promovido a nível pleno recentemente, mexe com uma técnologia que adora, 
-    cursa faculdade, tem bons amigos, um núcleo famíliar, é saudável e tem um lar) sente é inacreditável, parece que nossa vida está realmente "boa" e todo nosso 
-    esforço está finalmente sendo pago, o salário que recebemos não é aqueles que sonhamos quando entramos na área mas é o suficiente
-e mais que muitas famílias recebem, finalmente sentimos que ganhamos. Entretanto a programação é uma área volátil e muitas vezes demora para percebermos. 
-Ficamos muito tempo alucinados por nossa técnologia e nos fechamos a outras oportunidade; Recebemos muitos elogios e deixamos de estudar porque "já temos o suficiente"; 
-Ignoramos novas técnologia porquê as nossas "fazem o serviço";
-Entramos no que chamam de estagnação.</p><br/><p> Estagnar não significa deixar de aprender, eu ainda aprendo algo novo sempre que começo 
-um novo projeto, cada deploy me ensina algo de diferente e todo projeto entregue me trás experiência, porém nesses últimos mêses 
+<p style="text-indent: 5vw;">O conforto que senti por alguns mêses foi inacreditável, por ter aprendido muito rápido conseguia facilmente entregar tudo que era passado para mim, foram incontáveis 
+    os dias que após 1:30h ~ 2h de trampo eu tinha terminado todas minhas tasks e podia desligar, o salário sempre caía no dia certo e por não ser responsável de ninguém 
+    depois das contas pagas sobrava uma boa quantia para me divertir. Foi e é uma vida ótima, vocês nunca verão eu reclamar de nada nesse blog, eu sou uma pessoa privilegiada 
+    por ter tudo e todos que tenho, é surreal ver tantas pessoas incríveis sendo desligadas de outras empresas enquanto por muitas vezes recebi por fazer um trabalho 
+    mediano, acredito que essa sensação de conforto se tornou em angústia conforme o tempo e fez eu refletir sobre o momento que estava inserido. 
+    Fiquei muito tempo alucinados por minha tecnologia e me fechei a outras oportunidade; recebi muitos elogios e deixei de estudar porque 
+    "já tinha o suficiente"; alcancei o que entendi como estagnação pelo conforto.  
+</p>
+<p style="text-indent: 5vw;">
+    Não podemos nos confundir essa zona de conforto com estabilidade, acredito que todos almejam a estabilidade mas o que ocorreu comigo 
+    foi uma grande decaída na minha curva de aprendizado, aprendi "tudo" que era requisitado no meu trabalho e fiquei por aí, eu ainda aprendia algo novo sempre que começava 
+um novo projeto, cada deploy me ensinava algo de diferente e todo projeto entregue me trazia experiência, porém nesses últimos mêses 
 senti que eu não estava realmente sendo desafiado, comecei a ficar insatisfeito com o meu código, comecei a dúvidar do meu próprio cargo e seu merecimento, 
-a síndrome do impostor é algo que provavelmente todo desenvolvedor vai sentir, mesmo que em situações diferentes, e nesse momento temos a opção de deixar o conforto (ou ansiedade em alguns casos) nos engolir 
-ou partir para a próxima.</p> 
+a síndrome do impostor é algo que provavelmente todo desenvolvedor vai sentir, mesmo que em situações diferentes, e nesse momento temos a opção de deixar o conforto (e ansiedade em alguns casos) nos engolir 
+ou partir para a próxima.
+</p>
 <br/>
-<h2>O próximo passo</h2>
-<p>De fato o mais difícil na minha opinião já passou, é bem raro ver alguém que chegou "tão longe" na área desistir e ir para outra mas acredito que esse próximo é um passo 
+<h2>O meu próximo passo</h2>
+<p style="text-indent: 5vw;">De fato o mais difícil na minha opinião já passou, é bem raro ver alguém que chegou "tão longe" na área desistir e ir para outra mas acredito que esse próximo é um passo 
     crucial a ser tomado, eu não vou finalizar esse artigo dizendo oquê eu ou você deve fazer, eu realmente não sei mas acho que ser auto consciente é um ótimo início. 
     No último mês fiz 3 processos seletivos para "testar" como eu me encaixava no mercado, nos 3 eu fui barrado na entrevista técnica, na primeira eu fui genuinamente mal
     no teste prático, em outra tivemos problemas por conta da distância da empresa (a vaga era presencial) e na última (a que eu estava mais confiante) não fui chamado por não ser o melhor encaixe 
     na vaga, mesmo assim absorvi muito do que falta em mim atualmente como desenvolvedor, acredito que o próximo passo que irei tomar é esse, descobrir coisas novas. 
-    Vou criar e estudar as coisas que acreditava "não ser necessárias", mesmo meu Laravel "dando conta" vou aprender novas técnologias como Inertia e novos frameworks 
-    como Yii, vou me permitir errar e aprender, vou ser o que um desenvolvedor deveria ser desde o início: Um curioso.
+    Vou criar e estudar as coisas que acreditava "não ser necessárias", mesmo meu Laravel "dando conta" vou aprender novas técnologias, 
+    vou ler muita teoria de código que considerava bobeira, vou me permitir errar e aprender, vou ser o que um desenvolvedor deveria ser desde o início: Um curioso.
 </p>
 <br/>
 <h2>Conclusão</h2>
-<p>Tudo oque eu disse aqui pode ser uma grande baboseira, eu defini isso como um artigo mas não é nada mais que uma página em um diário de um desenvolvedor qualquer, mas acredito 
-    que esse é o ponto. No dia 27/05/2022 eu estarei pedindo demissão da minha atual empresa e começarei a procurar uma nova oportunidade, espero que tudo dê certo comigo e que dê certo para você, leitor.<br/>
+<p style="text-indent: 5vw;">Tudo oque eu disse aqui pode ser uma grande baboseira, eu defini isso como um artigo mas não é nada mais que uma página em um diário de um desenvolvedor qualquer, mas acredito 
+    que esse é o ponto. Nessa primeira semana de Junho de 2022 estarei entrando no processo de demissão da minha atual empresa e começarei a procurar uma nova oportunidade, espero que tudo dê certo comigo e que dê certo para você, leitor.<br/>
     Espero que tenham gostado e até a próxima!
 </p>
 <br/>
