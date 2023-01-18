ebaa02c502763434ba1864b5aeca03656bcf307d
dgtyPedro <pedrogcs0104@gmail.com>
2023-01-18 13:20:41 -0300

    translation post

--- resources/views/posts/4_o_meu_proximo_passo.blade.php
+++ resources/views/posts/4_o_meu_proximo_passo.blade.php
@@ -1,68 +1,65 @@
 @extends('index')
 @section('title')
-O meu próximo passo como desenvolvedor [PT-BR]
+    My next step as a developer
 @endsection
 @section('content')
-<p>Eu pretendia começar a postar só em inglês nesse blog, mas por conta de ser uma postagem com cunho pessoal vou estar escrevendo 
-    em português mesmo, espero que gostem do texto.
-</p>
-<style>p{margin:0!important;}</style>
+    <style>p{margin:0!important;}</style>
 <br/>
-<h2>O Background</h2>
+<h2>The Background</h2>
 <p style="text-indent: 5vw;">
-    Olá, sou o Pedro e hoje vou estar contando um pouco de um sentimento que vem me assombrando a um certo tempo.
-    Trabalho atualmente como Fullstack Web Developer em uma startup de soluções de TI, tive um crescimento notório nos meus primeiros meses e rapidamente
-    conquistei meu lugar na empresa. Quando entrei, ela havia acabado de ser criada e isso culminou em eu, um dev Júnior na época, receber 
-    grandes responsabilidades/tarefas. Quando essa situação ocorre ou o desenvolvedor não aguenta a pressão e acaba sendo desligado ou ele 
-    segura o tranco e ganha posição de destaque nessa empresa, esse último caso foi oque aconteceu comigo, não acho que consegui 
-    por eu ser necessariamente um gênio e sim por ter uma facilidade em aprender coisas no "calor do momento". Com isso 
-    recebi um certo prestígio dentro da empresa e passei a tocar projetos sozinhos, tenho a flexibilidade de fazer o que quiser 
-    quando quiser e tenho o conforto de saber que tudo está dentro do meu controle, senti por um momento que o pior no processo de virar um desenvolvedor já tinha passado.
-    Acredito que essa é a história de muitos desenvolvedores e que grande parte, assim como eu, passa por essa situação.
+    Hello, I'm Pedro and today I'm going to be telling you a little about a feeling that has been haunting me for a while.
+    I currently work as a Fullstack Web Developer in an IT solutions startup, I had a noticeable growth in my first months and quickly
+    I earned my place in the company. When I joined, it had just been created and that culminated in me, a Junior dev at the time, receiving
+    big responsibilities/tasks. When this situation occurs either the developer can't handle the pressure and ends up being shut down or he
+    holds the stride and gains a prominent position in this company, this last case is what happened to me, I don't think I made it
+    because I am necessarily a genius and yes because I have an ability to learn things in the "heat of the moment". Thereby
+    I received a certain amount of prestige within the company and started to run projects on my own, I have the flexibility to do whatever I want
+    whenever I want and I have the comfort of knowing that everything is within my control, I felt for a moment that the worst in the process of becoming a developer was over.
+    I believe that this is the story of many developers and that most of them, like me, go through this situation.
 </p>
 <br/>
-<h2>A situação</h2>
-<p style="text-indent: 5vw;">Por mais que a programação seja uma área
-    altamente procurada nos dias atuais fica cada vez mais difícil conseguir uma oportunidade de emprego no início, 
-a concorrência é extremamente alta para essas vagas de nível Júnior e já se foram os tempos onde um 
-certificado e vontade de aprender bastava, para realmente entrar no jogo se vão muitas horas de estudo e de projetos, eu passei por isso e sei que  
-quando de fato conseguimos a primeira oportunidade; o primeiro contrato; o primeiro dia de pagamento; nós desenvolvedores ficamos extasiados. Agora a parte 
-mais difícil que era conseguir uma oportunidade e se manter nela já foi, já se provamos e temos que nos preocupar "somente" 
-em trabalhar e mostrar serviço, pelo menos é o que pensamos, e é aí que surge uma grande armadilha que muitos caem. A estagnação pelo conforto. 
+<h2>The situation</h2>
+<p style="text-indent: 5vw;">As much as programming is an area
+    highly sought after these days it gets harder and harder to get a job opportunity at first,
+    competition is extremely high for these Junior level positions and gone are the days when a
+    certificate and willingness to learn was enough, to get into the game many hours of study and projects go by, I went through it and I know that
+    when we got the first opportunity; the first contract; the first payday; we developers were ecstatic. Now the part
+    more difficult than it was to get an opportunity and keep it, it's gone, we've already proved ourselves and we have to worry about "only"
+    in working and showing service, at least that's what we think, and that's where a big trap that many fall into.
+    Stagnation for comfort.
 </p>
 <br/>
-<h2>O problema</h2>
-<p style="text-indent: 5vw;">O conforto que senti por alguns meses foi inacreditável, por ter aprendido muito rápido conseguia facilmente entregar tudo que era passado para mim, foram incontáveis 
-    os dias que após 1:30h ~ 2h de trampo eu tinha terminado todas minhas tasks e podia desligar, o salário sempre caía no dia certo e por não ser responsável de ninguém 
-    depois das contas pagas sobrava uma boa quantia para me divertir. Foi e é uma vida ótima, vocês nunca verão eu reclamar de nada nesse blog, eu sou uma pessoa privilegiada 
-    por ter tudo e todos que tenho, é surreal ver tantas pessoas incríveis sendo desligadas de outras empresas enquanto por muitas vezes recebi por fazer um trabalho 
-    mediano, acredito que essa sensação de conforto se tornou em angústia conforme o tempo e fez eu refletir sobre o momento que estava inserido. 
-    Fiquei muito tempo alucinados por minha tecnologia e me fechei a outras oportunidade; recebi muitos elogios e deixei de estudar porque 
-    "já tinha o suficiente"; alcancei o que entendi como estagnação pelo conforto.  
+<h2>The problem</h2>
+<p style="text-indent: 5vw;">The comfort I felt for a few months was unbelievable, because I learned so quickly I could easily deliver everything that was passed on to me, there were countless
+    the days that after 1:30h ~ 2h of work I had finished all my tasks and could switch off, the salary always fell on the right day and for not being responsible for anyone
+    after the bills were paid, there was a good amount left over to have fun. It was and is a great life, you'll never see me complain about anything on this blog, I'm a privileged person
+    for having everything and everyone that I have, it's surreal to see so many amazing people being fired from other companies while many times I got paid for doing a job
+    average, I believe that this feeling of comfort turned into anguish over time and made me reflect on the moment that was inserted.
+    I was hallucinated by my technology for a long time and closed myself to other opportunities; I received many compliments and I stopped studying because
+    "I've had enough"; I achieved what I understood as stagnation through comfort.
 </p>
 <p style="text-indent: 5vw;">
-    Não podemos confundir essa zona de conforto com estabilidade, acredito que todos almejam a estabilidade mas o que ocorreu comigo 
-    foi uma grande decaída na minha curva de aprendizado, aprendi "tudo" que era requisitado no meu trabalho e fiquei por aí, eu ainda aprendia algo novo sempre que começava 
-um novo projeto, cada deploy me ensinava algo de diferente e todo projeto entregue me trazia experiência, porém nesses últimos meses 
-senti que eu não estava realmente sendo desafiado, comecei a ficar insatisfeito com o meu código, comecei a dúvidar do meu próprio cargo e seu merecimento, 
-a síndrome do impostor é algo que provavelmente todo desenvolvedor vai sentir, mesmo que em situações diferentes, e nesse momento temos a opção de deixar o conforto (e ansiedade em alguns casos) nos engolir 
+    Não podemos confundir essa zona de conforto com estabilidade, acredito que todos almejam a estabilidade mas o que ocorreu comigo
+    foi uma grande decaída na minha curva de aprendizado, aprendi "tudo" que era requisitado no meu trabalho e fiquei por aí, eu ainda aprendia algo novo sempre que começava
+um novo projeto, cada deploy me ensinava algo de diferente e todo projeto entregue me trazia experiência, porém nesses últimos meses
+senti que eu não estava realmente sendo desafiado, comecei a ficar insatisfeito com o meu código, comecei a dúvidar do meu próprio cargo e seu merecimento,
+a síndrome do impostor é algo que provavelmente todo desenvolvedor vai sentir, mesmo que em situações diferentes, e nesse momento temos a opção de deixar o conforto (e ansiedade em alguns casos) nos engolir
 ou partir para a próxima.
 </p>
 <br/>
-<h2>O meu próximo passo</h2>
-<p style="text-indent: 5vw;">De fato o mais difícil na minha opinião já passou, é bem raro ver alguém que chegou "tão longe" na área desistir e ir para outra mas acredito que esse próximo é um passo 
-    crucial a ser tomado, eu não vou finalizar esse artigo dizendo oquê eu ou você deve fazer, eu realmente não sei mas acho que ser auto consciente é um ótimo início. 
-    No último mês fiz 3 processos seletivos para "testar" como eu me encaixava no mercado, nos 3 eu fui barrado na entrevista técnica, na primeira eu fui genuinamente mal
-    no teste prático, em outra tivemos problemas por conta da distância da empresa (a vaga era presencial) e na última (a que eu estava mais confiante) não fui chamado por não ser o melhor encaixe 
-    na vaga, mesmo assim absorvi muito do que falta em mim atualmente como desenvolvedor, acredito que o próximo passo que irei tomar é esse, descobrir coisas novas. 
-    Vou criar e estudar as coisas que acreditava "não ser necessárias", mesmo meu Laravel "dando conta" vou aprender novas técnologias, 
-    vou ler muita teoria de código que considerava bobeira, vou me permitir errar e aprender, vou ser o que um desenvolvedor deveria ser desde o início: Um curioso.
+<h2>My next step</h2>
+<p style="text-indent: 5vw;">We cannot confuse this comfort zone with stability, I believe that everyone wants stability, but what happened to me
+    it was a big drop in my learning curve, I learned "everything" that was required in my job and that was it, I still learned something new every time I started
+    a new project, each deployment taught me something different and every project delivered brought me to experience, but in these last few months
+    I felt that I wasn't really being challenged, I started to become dissatisfied with my code, I started to doubt my own position and its worthiness,
+    imposter syndrome is something that probably every developer will feel, even if in different situations, and at that moment we have the option of letting comfort (and anxiety in some cases) swallow us
+    or move on to the next one.
 </p>
 <br/>
-<h2>Conclusão</h2>
-<p style="text-indent: 5vw;">Tudo oque eu disse aqui pode ser uma grande baboseira, eu defini isso como um artigo mas não é nada mais que uma página em um diário de um desenvolvedor qualquer, mas acredito 
-    que esse é o ponto. Nessa primeira semana de Junho de 2022 estarei entrando no processo de demissão da minha atual empresa e começarei a procurar uma nova oportunidade, espero que tudo dê certo comigo e que dê certo para você, leitor.<br/>
-    Espero que tenham gostado e até a próxima!
+<h2>Conclusion</h2>
+<p style="text-indent: 5vw;">Everything I said here could be a lot of bullshit, I defined it as an article but it's nothing more than a page in a developer's diary, but I believe
+    that that's the point. In this first week of June 2022, I will be starting the process of resigning from my current company and will start looking for a new opportunity, I hope that everything works out for me and that it works for you, reader.<br/>
+    I hope you enjoyed it and see you soon!
 </p>
 <br/>
 @endsection
